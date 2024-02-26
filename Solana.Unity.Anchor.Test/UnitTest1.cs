04b46828c10358e9a81d780859e4e3832705de66
Gabriele Picco <piccogabriele@gmail.com>
2024-02-26 12:40:01 +0100

    :bug: Fix generation for f32 and f64 types

--- Solana.Unity.Anchor.Test/UnitTest1.cs
+++ Solana.Unity.Anchor.Test/UnitTest1.cs
@@ -5,11 +5,41 @@ namespace Solana.Unity.Anchor.Test
     [TestClass]
     public class UnitTest1
     {
+        
+        [TestMethod]
+        public void TestEmptyIdl()
+        {
+            var res = IdlParser.ParseFile("Resources/Empty.json");
+            Assert.IsNotNull(res);
+
+            ClientGenerator c = new();
+
+            c.GenerateSyntaxTree(res);
+            Assert.IsNotNull(c);
+
+            var code = c.GenerateCode(res);
+            Assert.IsNotNull(code);
+        }
+        
         [TestMethod]
         public void TestMethod1()
         {
-            //var res = IdlParser.ParseFile("Resources/ChatExample.json");
             var res = IdlParser.ParseFile("Resources/seq.json");
+            Assert.IsNotNull(res);
+
+            ClientGenerator c = new();
+
+            c.GenerateSyntaxTree(res);
+            Assert.IsNotNull(c);
+
+            var code = c.GenerateCode(res);
+            Assert.IsNotNull(code);
+        }
+        
+        [TestMethod]
+        public void TestIdlWithOptional()
+        {
+            var res = IdlParser.ParseFile("Resources/SolChess.json");
             Assert.IsNotNull(res);
 
             ClientGenerator c = new();
